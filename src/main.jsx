f632414cf2adb10041c5085f0162cb788d116a19
Soundharmuthu <soundharmuthu06@gmail.com>
2026-07-02 19:52:59 +0530

    tm-content-front-2

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ContentReportDashboard from './ContentReportDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContentReportDashboard />
+  </React.StrictMode>
+)
